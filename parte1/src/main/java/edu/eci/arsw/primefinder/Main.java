629b4edb36eb30fbd53f645849c17f5a3f3fe1ee
Cristhian <cristhian.torres@mail.escuelaing.edu.co>
2021-08-19 20:48:10 -0500

    lab2 punto1

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,26 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
+
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		PrimeFinderThread philo = new PrimeFinderThread(0, 9999999);
+		PrimeFinderThread shilo = new PrimeFinderThread(10000000, 19999999);
+		PrimeFinderThread thilo = new PrimeFinderThread(20000000, 30000000);
+		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		long inicio = System.currentTimeMillis();
+		philo.start();
+		shilo.start();
+		thilo.start();
+		if (inicio == 500) {
+			System.out.println("Entra al condicional");
+			philo.interrupt();
+			shilo.interrupt();
+			thilo.interrupt();
+		}
+
 	}
-	
 }
